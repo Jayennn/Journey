3077c088999194c8c1bb420ab32d93a39d57b6c8
Jayenn <108043334+Jayennn@users.noreply.github.com>
2025-02-14 03:29:22 +0000

    feat: analytics vercel

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import type { AppProps } from "next/app";
 import Lenis from "lenis";
 import { AnimatePresence, motion } from "motion/react";
 import { useRouter } from "next/router";
+import { Analytics } from "@vercel/analytics/react";
 
 export type NextPageWithLayout<P = object, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -16,7 +17,6 @@ type AppPropsWithLayout = AppProps & {
 
 export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   const router = useRouter();
-
   const getLayout = Component.getLayout ?? ((page) => page);
 
   useEffect(() => {
@@ -34,16 +34,22 @@ export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
       requestAnimationFrame(raf);
     }
 
-    requestAnimationFrame(raf);
+    const rafId = requestAnimationFrame(raf);
+
+    return () => {
+      cancelAnimationFrame(rafId);
+      lenis.destroy();
+    };
   }, []);
 
   return getLayout(
-    <AnimatePresence mode="wait">
+    <AnimatePresence mode="wait" initial={false}>
       <motion.div key={router.pathname}>
         <Component {...pageProps} />
+        <Analytics />
         <motion.div
           className="slide-in absolute top-0 left-0 w-full h-screen bg-black origin-bottom z-50"
-          initial={{ scaleY: 0, }}
+          initial={{ scaleY: 0 }}
           animate={{ scaleY: 0 }}
           exit={{ scaleY: 1 }}
           transition={{ duration: 1, ease: [0.22, 1, 0.36, 1] }}
